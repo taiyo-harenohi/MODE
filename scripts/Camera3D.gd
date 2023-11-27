7b4f6312b0cb56ebaa10fc498392add2f9d64cfa
Nikola Machálková <hare.no.hi.official@gmail.com>
2023-11-27 21:02:13 +0100

    Add detection of clicked item

--- scripts/Camera3D.gd
+++ scripts/Camera3D.gd
@@ -8,11 +8,16 @@ var dragSen: float = 2.0
 var center: Vector3 = Vector3(1, 2, 2.5)
 var zoom: float = 45
 
+var mouse = Vector3()
+
 func _input(event):
 	if event is InputEventMouseMotion and Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
 		position -= Vector3(event.relative.x, 0, event.relative.y) * dragSen / fov * 0.1
 
 	if event is InputEventMouseButton:
+		mouse = event.position
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			get_selected()
 		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
 			fov -= zoomSpeed
 		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
@@ -24,3 +29,10 @@ func _input(event):
 		position = center
 	if Input.is_action_just_pressed("restart_zoom"):
 		fov = zoom
+
+func get_selected():
+	var worldspace = get_world_3d().direct_space_state
+	var start = project_ray_origin(mouse)
+	var end = project_position(mouse, 1000)
+	var result = worldspace.intersect_ray(PhysicsRayQueryParameters3D.create(start, end))
+	print(result["collider"])
